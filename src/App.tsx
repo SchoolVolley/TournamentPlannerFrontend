3189df60355233b83b2503c050482caafb0acb31
SchoolVolley <125000068+SimonTrummer@users.noreply.github.com>
2023-04-25 16:19:13 +0200

    Design Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Sidebar from "./Components/sidebar/Sidebar";
+import Dashboard from "./Views/dashboard/Dashboard";
+import Teams from "./Views/teams/Teams";
+import Referees from "./Views/referees/Referees";
+import Statistics from "./Views/statistics/Statistics";
+import Matches from "./Views/matches/Matches";
+import Settings from "./Views/settings/Settings";
+
+import "./app.css"
 
 function App() {
     document.body.classList.add("no-scroll")
@@ -7,7 +16,17 @@ function App() {
 
     return (
         <BrowserRouter>
-
+            <Sidebar>
+                <Routes>
+                    <Route path="/" element={<Dashboard></Dashboard>}></Route>
+                    <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+                    <Route path="/teams" element={<Teams></Teams>}></Route>
+                    <Route path="/referees" element={<Referees></Referees>}></Route>
+                    <Route path="/statistics" element={<Statistics></Statistics>}></Route>
+                    <Route path="/matches" element={<Matches></Matches>}></Route>
+                    <Route path="/settings" element={<Settings></Settings>}></Route>
+                </Routes>
+            </Sidebar>
         </BrowserRouter>
     );
 }
---
+++ src/Components/sidebar/Sidebar.tsx
@@ -0,0 +1,87 @@
+import {
+    AiFillHome, AiFillSetting, AiOutlineTeam,
+} from "react-icons/ai";
+import {
+    GiWhistle,
+}from "react-icons/gi"
+import {
+    MdOutlineSportsVolleyball,
+}from "react-icons/md"
+import{
+    VscGraph
+}from "react-icons/vsc"
+import {NavLink} from "react-router-dom";
+import {ReactNode, useState} from "react";
+import "./sidebar.css"
+import CSS from 'csstype'
+
+interface Props {
+    children?: ReactNode
+    // any props that come into the component
+}
+const Sidebar = ({children,...props}:Props) => {
+
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const styleOpen: CSS.Properties = {
+        width: '30vh'
+    }
+    const styleClosed: CSS.Properties = {
+        width: '8vh'
+    }
+
+    const menuItem = [
+        {
+            path: "/",
+            name: "Dashboard",
+            icon: <AiFillHome></AiFillHome>
+        },
+        {
+            path: "/teams",
+            name: "Teams",
+            icon: <AiOutlineTeam></AiOutlineTeam>
+        },
+        {
+            path: "/referees",
+            name: "Referees",
+            icon:  <GiWhistle></GiWhistle>
+        },
+        {
+            path: "/statistics",
+            name: "Statistics",
+            icon: <VscGraph></VscGraph>
+        },
+        {
+            path: "/matches",
+            name: "Matches",
+            icon: <MdOutlineSportsVolleyball></MdOutlineSportsVolleyball>
+        },
+        {
+            path: "/settings",
+            name: "Settings",
+            icon: <AiFillSetting></AiFillSetting>
+        },
+    ]
+
+    return (
+        <div className="sidebar-container">
+            <div className="sidebar" style={isOpen?styleOpen:styleClosed} onMouseOverCapture={() => setIsOpen(true)} onMouseOutCapture={() => setIsOpen(false)}>
+                {
+                    menuItem.map((item,index)=>(
+                        <NavLink to={item.path} key={index} className="sidebar-navlink">
+                            <div className="sidebar-navlink-icon">
+                                {item.icon}
+                            </div>
+                            <div style={{display: isOpen ? 'block': "none"}} className="sidebar-navlink-text">
+                                {item.name}
+                            </div>
+                        </NavLink>
+                    ))
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
