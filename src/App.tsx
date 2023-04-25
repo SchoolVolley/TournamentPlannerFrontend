46660feede09c4873b92e616dcb13bbd647f35e3
SchoolVolley <125000068+SimonTrummer@users.noreply.github.com>
2023-04-25 09:27:20 +0200

    1st

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+function App() {
+    document.body.classList.add("no-scroll")
+
+
+    return (
+        <BrowserRouter>
+
+        </BrowserRouter>
+    );
+}
+
+export default App;
