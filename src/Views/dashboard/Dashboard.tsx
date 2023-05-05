8211cb50325b949cdeffdfc39afc21e1ae115a10
SchoolVolley <125000068+SimonTrummer@users.noreply.github.com>
2023-05-05 09:32:41 +0200

    Group Dashboard.tsx

--- src/Views/dashboard/Dashboard.tsx
+++ src/Views/dashboard/Dashboard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./dashboard.css"
 import MatchSmall from "../../Components/match/MatchSmall";
 import {NavLink} from "react-router-dom";
+import {BiLeftArrow, BiRightArrow} from "react-icons/bi";
 
 const Dashboard = () => {
     return (
@@ -11,7 +12,7 @@ const Dashboard = () => {
             </div>
             <div className="container-dashboard-right container-positioner">
                 <div className="container-dashboard-nextmatches">
-                    <NavLink to={'/matches'} style={{textDecoration:'none'}}>
+                    <NavLink to={'/matches'} style={{textDecoration: 'none'}}>
                         <h1>Running Matches</h1>
                     </NavLink>
                     <hr/>
@@ -23,6 +24,41 @@ const Dashboard = () => {
                     <MatchSmall/>
                 </div>
                 <div className="container-dashboard-groups">
+                    <h1>Groups</h1>
+                    <hr/>
+                    <div className="dashboard-group-switch">
+                        <BiLeftArrow className={"dashboard-group-switch-icon"}></BiLeftArrow>
+                        <h3>
+                            Group A
+                        </h3>
+                        <BiRightArrow className={"dashboard-group-switch-icon"}></BiRightArrow>
+                    </div>
+                    <div className="dashboard-groups-table">
+                        <table>
+                            <thead>
+                            <tr>
+                                <th style={{width: '3rem'}}>Pos</th>
+                                <th style={{width: '40%'}}>Team</th>
+                                <th style={{width: '3rem'}}>P</th>
+                                <th style={{width: '3rem'}}>W</th>
+                                <th style={{width: '3rem'}}>L</th>
+                                <th style={{width: '3rem'}}>G</th>
+                                <th>PD</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td><NavLink to={'/teams'}>VCH</NavLink></td>
+                                <td>10</td>
+                                <td>5</td>
+                                <td>3</td>
+                                <td>8</td>
+                                <td>153:180</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
 
                 </div>
             </div>
