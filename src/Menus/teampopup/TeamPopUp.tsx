1c5d18d03cf93d0dca7b5081e2d5e53ca86b38fd
davedangee <leidac20@htl-kaindorf.at>
2023-05-07 13:42:27 +0200

    David

--- src/Menus/teampopup/TeamPopUp.tsx
+++ src/Menus/teampopup/TeamPopUp.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./teampopup.css"
 import PopUpMenu from "../../Components/popupmenu/PopUpMenu";
+import {AiOutlineTeam} from "react-icons/ai";
 
 interface Props {
     onClose:()=> void;
@@ -9,6 +10,7 @@ interface Props {
 const TeamPopUp:React.FC<Props>= ({onClose}) => {
     return (
         <PopUpMenu dropShadow={true} height={50} width={70} onClose={onClose} >
+            <div className={"team-small-name"}>VC Hausmannstätten <hr></hr></div>
 
         </PopUpMenu>
     );
