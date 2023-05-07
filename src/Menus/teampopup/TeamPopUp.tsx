bab3856a336f049a502d34a5d4029c2066f5d6b3
davedangee <leidac20@htl-kaindorf.at>
2023-05-07 15:44:25 +0200

    d

--- src/Menus/teampopup/TeamPopUp.tsx
+++ src/Menus/teampopup/TeamPopUp.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import "./teampopup.css"
 import PopUpMenu from "../../Components/popupmenu/PopUpMenu";
-import {AiOutlineTeam} from "react-icons/ai";
 
 interface Props {
     onClose:()=> void;
@@ -10,7 +9,33 @@ interface Props {
 const TeamPopUp:React.FC<Props>= ({onClose}) => {
     return (
         <PopUpMenu dropShadow={true} height={50} width={70} onClose={onClose} >
-            <div className={"team-small-name"}>VC Hausmannstätten <hr></hr></div>
+            <div className={"team-popup-header"}>VC Hausmannstätten <hr></hr></div>
+
+            <div className={"wrapper"}>
+
+
+                <div className={"container-picture"}>
+                    <img className={"placeholder"} src={"https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg"} alt={""}/>
+                </div>
+
+
+                <div className={"container-team-leader"}>
+                    <div className={"team-leader-header"}>
+                        Team leader
+                    </div>
+                    <p>Firstname: Michael</p>
+                    <p>Lastname: Hubenauf</p>
+                    <p>Email: example@gmail.com</p>
+                </div>
+
+dfd
+                <div className={"container-games"}>
+                    <div className={"team-leader-header"}>
+                        Games
+                    </div>
+                </div>
+
+            </div>
 
         </PopUpMenu>
     );
